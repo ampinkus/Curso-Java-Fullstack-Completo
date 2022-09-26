037fb5985479c21e522732a5f2ea6c5f2c78aac8
Alfredo Pinkus <ampinkus@gmail.com>
2022-09-26 17:49:22 -0300

    Added first resource

--- curso/src/main/java/com/cursojava/curso/controlers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controlers/UsuarioController.java
@@ -3,11 +3,13 @@ package com.cursojava.curso.controlers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "Si ves la palabra Prueba es que el controlador está funcionando";
+    public List<String> prueba(){
+        return List.of("Manzana", "Naranja", "Kiwi");
     }
 }
