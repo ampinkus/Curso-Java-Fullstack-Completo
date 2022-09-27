d0590e22c778f9340697d1e4eedd66a8731c8c65
Alfredo Pinkus <ampinkus@gmail.com>
2022-09-27 16:35:04 -0300

    Added lombok

--- curso/src/main/resources/static/js/usuarios.js
+++ curso/src/main/resources/static/js/usuarios.js
@@ -16,7 +16,7 @@ async function cargarUsuarios() {
     const usuarios = await request.json();
     let listadoHtml = '';
     for (let usuario of usuarios) {
-        let usuarioHtml = '<tr><td>ID</td><td>' + usuario.nombre + '  ' + usuario.apellido + '</td><td>'
+        let usuarioHtml = '<tr><td>'+ usuario.id +'</td><td>' + usuario.nombre + '  ' + usuario.apellido + '</td><td>'
             + usuario.email + '</td><td>' + usuario.telefono + '</td><td><a href= "#" class= "btn btn-danger btn-circle btn-sm"><i class= "fas fa-trash"></i></a></td></tr>'
         listadoHtml += usuarioHtml; // armamos un string con todo el listado de usuarios
     }
